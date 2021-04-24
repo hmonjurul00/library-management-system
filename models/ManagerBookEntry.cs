6cbf375aecb0bbb5be3a3544aaca8ed17de24d85
hmonjurul00 <hmonjurul00@gmail.com>
2021-04-25 01:32:45 +0600

    add managerBookEntry

--- models/ManagerBookEntry.cs
+++ models/ManagerBookEntry.cs
@@ -12,6 +12,63 @@ namespace library_management_system
             set { bookId = value; }
         }
 
+        private string bookName;
+
+        public string BookName
+        {
+            get { return bookName; }
+            set { bookName = value; }
+        }
+
+        private int bookPublishYear;
+
+        public int BookPublishYear
+        {
+            get { return bookPublishYear; }
+            set { bookPublishYear = value; }
+        }
+
+        private string writerName;
+
+        public string WriterName
+        {
+            get { return writerName; }
+            set { writerName = value; }
+        }
+
+        private int quantityOfBook;
+
+        public int QuantityOfBook
+        {
+            get { return quantityOfBook; }
+            set { quantityOfBook = value; }
+        }
+
+        private string catagoryName;
+
+        public string CatagoryName
+        {
+            get { return catagoryName; }
+            set { catagoryName = value; }
+        }
+
+        private DateTime entryDate;
+
+        public DateTime EntryDate
+        {
+            get { return entryDate; }
+            set { entryDate = value; }
+        }
+
+        private Boolean availableBook;
+
+        public Boolean AvailableBook
+        {
+            get { return availableBook; }
+            set { availableBook = value; }
+        }
         public ManagerBookEntry() { }
+
+        public ManagerBookEntry(int _bookId, string _bookName, int _bookPublishYear) { }
     }
 }
