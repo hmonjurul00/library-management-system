41d8a814c9f28c12dd783d2c07619ffd7ba72be0
hmonjurul00 <hmonjurul00@gmail.com>
2021-04-25 00:19:34 +0600

    Create view forms

--- views/UserForm.Designer.cs
+++ views/UserForm.Designer.cs
@@ -1,15 +1,15 @@
 ﻿
-namespace library_management_system
+namespace library_management_system.views
 {
-    partial class Form1
+    partial class UserForm
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,18 +24,24 @@ namespace library_management_system
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "UserForm";
+            this.Text = "UserForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
