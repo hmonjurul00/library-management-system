319ed06cbed781596b72aa2b1593d5afd5fb16e8
hmonjurul00 <hmonjurul00@gmail.com>
2021-04-30 16:50:38 +0600

    Add book table view

--- views/ManagerBookSearchForm.cs
+++ views/ManagerBookSearchForm.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using db;
+using models;
 
 namespace library_management_system.views
 {
@@ -13,16 +15,37 @@ namespace library_management_system.views
         public ManagerBookSearchForm()
         {
             InitializeComponent();
+            loadData();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void ManagerBookSearchForm_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void label5_Click(object sender, EventArgs e)
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        public void loadData()
+        {
+            MySQLConnector mySQLConnector = new MySQLConnector();
+            List<ManagerBookEntry> managerBookEntries = mySQLConnector.getAllBooks();
+
+            foreach (ManagerBookEntry managerBookEntry in managerBookEntries)
+            {
+                string[] row = new string[] {
+                    managerBookEntry.BookId.ToString(),
+                    managerBookEntry.BookName,
+                    managerBookEntry.BookPublishYear.ToString(),
+                    managerBookEntry.WriterName,
+                    managerBookEntry.QuantityOfBook.ToString(),
+                    managerBookEntry.CatagoryName,
+                    managerBookEntry.AvailableBook.ToString()
+                };
+                this.dataGridView1.Rows.Add(row);
+            }
         }
     }
 }
